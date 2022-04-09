a29597452ec84d3789d259b07fd7510ef445a506
gabrielcmoura <gabriel.moura@gsw.com.br>
2022-04-09 13:41:05 -0300

    feat: home page with Header and feed component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import * as SC from './styles'
+import Header from '../../components/Header'
+import Feed from '../../components/Feed'
+
+const Home = () => {
+  return (
+    <SC.Home>
+      <Header />
+      <Feed />
+    </SC.Home>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import BackgroundImage from '../../assets/background.svg'
+
+export const Home = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${BackgroundImage});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 32px;
+`
